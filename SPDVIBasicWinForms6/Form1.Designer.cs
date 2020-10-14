9b5a5b0d45c0c041e9cccc0f8466d2f2c1ade0a2
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-10-14 20:49:32 +0200

    Created project SPDVIBasicWinForms6 In this project we have a Form with three RadioButton inside a Panel. When clicking the button we get the Text value of the checked RadioButton and concat it with the text "Choosen colour:". The full text is shown in the TextBox.

---
+++ SPDVIBasicWinForms6/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SPDVIBasicWinForms6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.whiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.blueRadioButton = new System.Windows.Forms.RadioButton();
+            this.redRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.redRadioButton);
+            this.panel1.Controls.Add(this.blueRadioButton);
+            this.panel1.Controls.Add(this.whiteRadioButton);
+            this.panel1.Location = new System.Drawing.Point(89, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 243);
+            this.panel1.TabIndex = 0;
+            // 
+            // whiteRadioButton
+            // 
+            this.whiteRadioButton.AutoSize = true;
+            this.whiteRadioButton.Location = new System.Drawing.Point(28, 35);
+            this.whiteRadioButton.Name = "whiteRadioButton";
+            this.whiteRadioButton.Size = new System.Drawing.Size(98, 29);
+            this.whiteRadioButton.TabIndex = 0;
+            this.whiteRadioButton.TabStop = true;
+            this.whiteRadioButton.Text = "White";
+            this.whiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // blueRadioButton
+            // 
+            this.blueRadioButton.AutoSize = true;
+            this.blueRadioButton.Location = new System.Drawing.Point(28, 107);
+            this.blueRadioButton.Name = "blueRadioButton";
+            this.blueRadioButton.Size = new System.Drawing.Size(86, 29);
+            this.blueRadioButton.TabIndex = 1;
+            this.blueRadioButton.TabStop = true;
+            this.blueRadioButton.Text = "Blue";
+            this.blueRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // redRadioButton
+            // 
+            this.redRadioButton.AutoSize = true;
+            this.redRadioButton.Location = new System.Drawing.Point(28, 175);
+            this.redRadioButton.Name = "redRadioButton";
+            this.redRadioButton.Size = new System.Drawing.Size(82, 29);
+            this.redRadioButton.TabIndex = 2;
+            this.redRadioButton.TabStop = true;
+            this.redRadioButton.Text = "Red";
+            this.redRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 470);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton redRadioButton;
+        private System.Windows.Forms.RadioButton whiteRadioButton;
+        private System.Windows.Forms.RadioButton blueRadioButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SPDVIBasicWinForms6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPDVIBasicWinForms6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var checkedButton = panel1.Controls.OfType<RadioButton>()
+                                      .FirstOrDefault(r => r.Checked);
+            string text = "Choosen colour: ";
+            textBox1.Text = text + checkedButton.Text;
+            Color bg = Color.FromName(checkedButton.Text);
+            button1.BackColor = bg;
+        }
+    }
+}
