c1e2780b7017f3800f7a5a85f44bf22f534b3a3d
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-10-15 18:56:35 +0200

    Created project SPDVIBasicWinForms9 Created a simple form with a combobox, two buttons and a textbox. The button Even insert even numbers on the combobox (from 0 to 10). The button Odd insert odd numbers on the combobox (from 0 to 10). When you click a number on the list the value is copied into the textbox.

---
+++ SPDVIBasicWinForms9/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPDVIBasicWinForms9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insertNumbers(true);
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            insertNumbers(false);
+        }
+        private void insertNumbers(bool even)
+        {
+            comboBox1.Items.Clear();
+            for(int i = 0; i<10; i++)
+            {
+                if (i % 2 == 0 && even)
+                    comboBox1.Items.Add("Nº" + i);
+                else if (i % 2 != 0 && !even)
+                    comboBox1.Items.Add("Nº" + i);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
