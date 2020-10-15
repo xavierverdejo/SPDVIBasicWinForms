098a4d03fff2ed83d0a4d870b7e431384fa7699b
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-10-15 18:45:02 +0200

    Created project SPDVIBasicWinForms8 I created a simple form with a ListBox, two buttons and a TextBox. When you click on ‘Classroom 1’, all the students of classroom 1 are into the listbox. The same for ‘Classroom 2’. The button ‘Clear List’ clears the listbox. When you click on some student, this student's name is copied into the textbox.

---
+++ SPDVIBasicWinForms8/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPDVIBasicWinForms8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Juan");
+            listBox1.Items.Add("Pepa");
+            listBox1.Items.Add("Pig");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Jose");
+            listBox1.Items.Add("Maria");
+            listBox1.Items.Add("Alejandro");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = listBox1.SelectedItem.ToString();
+            }
+            catch(Exception ex)
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
